25c9e10bbe12b64eb47ac7100158efe84cde3a39
= <cashrox@gmail.com>
2015-12-23 23:58:01 -0200

    adjust route

--- controller/process.js
+++ controller/process.js
@@ -7,10 +7,6 @@ var controller = require('../core/controller');
 
 process.getBreakPoints = (req, res, next) => {
     res.setHeader('Content-Type', 'application/json');
-    res.send('testeteste');
-  //  controller.go(req.body).then(function(data){
-    //    res.send(JSON.stringify(data));
-   // });
 };
 
 
