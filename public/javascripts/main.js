437ba80f20c011c28c97bb6dfdd9ce4b0f68093d
= <cashrox@gmail.com>
2015-12-26 14:16:10 -0200

    Revert "adjust forcedownload method"
    This reverts commit d77e52f9bec6f4e61ff5c904a0d7105e44edd7c6.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -167,7 +167,7 @@ Main.prototype.previous = function () {
     this.change();
 };
 
-Main.prototype.forceZipDownload = function (dir) {
+Main.prototye.forceZipDownload = function (dir) {
     window.location.href = '/' + dir + '.zip';
 };
 
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var controller = require('../controller');
 
 router.get('/', (req, res) => {
     res.render('index', {});
